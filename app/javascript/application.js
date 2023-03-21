2f1d017936fd1e483dc56ca4119d9de39e3ab5c3
arishavelle18 <arishavelle18@yahoo.com>
2023-03-21 22:20:29 +0800

    Fix Toyapp

--- app/javascript/application.js
+++ app/javascript/application.js
@@ -1,9 +1,4 @@
-import { Application } from "@hotwired/stimulus"
+// Configure your import map in config/importmap.rb. Read more: https://github.com/rails/importmap-rails
 
-const application = Application.start()
 
-// Configure Stimulus development experience
-application.debug = false
-window.Stimulus   = application
-
-export { application }
+console.log("Hello from application.js");
--- app/models/micropost.rb
+++ app/models/micropost.rb
@@ -18,7 +18,7 @@ class Micropost < ApplicationRecord
         end
     end
     def picture_as_thumbnail(pic)
-        pic.variant(resize_to_limit:[300,300]).processed
+        pic.variant(resize: '300x300').processed
     end
     
 end
