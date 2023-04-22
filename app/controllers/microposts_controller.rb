c3470abd2095131d7e9016c46cfe72922733c0a6
arishavelle18 <arishavelle18@yahoo.com>
2023-04-22 18:31:13 +0800

    converting multiple image using cloudinary

--- app/controllers/microposts_controller.rb
+++ app/controllers/microposts_controller.rb
@@ -65,11 +65,32 @@ class MicropostsController < ApplicationController
             redirect_to microposts_path,:alert => "Unauthorize Access"
         end
         respond_to do |format|
-            # if the length of the image is less than one meaning no images it will run without image params  else micropost params
-            if params[:micropost][:images].length <=1? @micropost.update(micropost_params_without_image):@micropost.update(micropost_params)
+            if @micropost.update(micropost_params)
+                # @micropost.image = "sad"
+                if params[:micropost][:images].length > 1 &&  JSON.parse(@micropost[:images].to_json)["images"].length > 0
+                    JSON.parse(@micropost[:images].to_json)["images"].each do |image|
+                        Cloudinary::Uploader.destroy(image) 
+                    end
+                end
+                
+                if !params[:micropost][:images].nil? && params[:micropost][:images].length > 1
+                    # add image
+                    
+                    images = { "images" => []}
+
+                    params[:micropost][:images].each do |image|
+                        
+                        if !image.blank?
+                            result = Cloudinary::Uploader.upload(image)
+                            # push
+                            images["images"] << result["public_id"]
+                        end
+                    end
+                    images = images
+                    @micropost.update_attribute("images",images)
+                end
                 flash[:notice] = "Micropost was successfully updated"
                 format.html { redirect_to micropost_path(@micropost) }
-               
             else
                 format.html { render :edit , status: :unprocessable_entity}
             end
@@ -86,6 +107,11 @@ class MicropostsController < ApplicationController
         else
             # find thee id of it
             @micropost = Micropost.find(params[:id])
+            if JSON.parse(@micropost[:images].to_json)["images"] != "images" && JSON.parse(@micropost[:images].to_json)["images"].length >  0 
+                JSON.parse(@micropost[:images].to_json)["images"].each do |image|
+                    Cloudinary::Uploader.destroy(image) 
+                end
+            end
             @micropost.destroy
             flash[:notice] = "User was successfully destroyed."
             redirect_to microposts_path
